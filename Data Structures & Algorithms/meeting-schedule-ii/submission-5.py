8ecc41909be89b1b4c4b2b114a02cb8e34f8e0d1
ronneylogs <104666332+ronneylogs@users.noreply.github.com>
2026-04-10 18:47:52 -0700

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,30 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x:x.start)
+
+        min_heap = []
+
+        for i in intervals:
+
+            # Can reuse the room
+            if min_heap and min_heap[0] <= i.start:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap,i.end)
+
+            # Cannot reuse the room
+            else:
+                heapq.heappush(min_heap,i.end)
+
+
+        
+
+        return len(min_heap)
+        
