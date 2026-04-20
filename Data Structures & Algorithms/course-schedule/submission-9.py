3718d4a9becb18e3458ec5620db4e31fbe018301
ronneylogs <104666332+ronneylogs@users.noreply.github.com>
2026-04-20 02:00:49 -0700

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        preMap = defaultdict(list)
+        for crs in prerequisites:
+            preMap[crs[0]].append(crs[1])
+
+        visited = set()
+        done = set()
+
+        def dfs(crs):
+            if crs in done:
+                return True
+            if crs in visited:
+                return False
+            
+            visited.add(crs)
+            for n in preMap[crs]:
+                if dfs(n) == False:
+                    return False
+
+            done.add(crs)
+            visited.remove(crs)
+            return True
+
+        
+        for crs in range(numCourses):
+            if dfs(crs) == False:
+                return False
+        
+        return True
+
+
+        
+
+        # create a dictionary
+            # {course: [prereqs]}
+        # iterate through each course then travel through each of the prereqs, i should not need to a visit 
+            # a course that is already visited
