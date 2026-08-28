370a7a5ec4fdd7eee3fe0052f16bb3fb9ed1a7bc
ronneylogs <104666332+ronneylogs@users.noreply.github.com>
2026-08-27 19:39:05 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        bracket_dict = {"[":"]","{":"}","(":")"}
+        stack = []
+        for i in range(len(s)):
+            if s[i] in bracket_dict:
+                stack.append(s[i])
+            else:
+                if len(stack) == 0 or s[i] != bracket_dict[stack[-1]]:
+                    return False
+                
+                
+                stack.pop()
+            
+        
+        if len(stack) == 0: return True
+
+        return False
+
+     
+
+
+
+# Idea
+# We travel from left to right of the string
+
+# If opening bracket, we append to stack
+# If closing bracket, we pop from stack and check if don't match
+    # If yes then we return false
+
+# At the end if the stack is not empty then we return false
